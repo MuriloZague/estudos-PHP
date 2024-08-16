33c791dcb1e687089eb09f5eb1b1803f2f88d95e
Murilo Brandini Zague <zaguemurilo7@gmail.com>
2024-08-16 14:15:31 -0300

    funcoesSaidas.php

--- funcoesSaidas.php
+++ funcoesSaidas.php
@@ -4,16 +4,16 @@
 echo "oiee" . "tudo bem?" . PHP_EOL;
 
 // var_dump - identifica o tipo de valor de algo (int, double, string) booleans (true e false)
-var_dump(value: "afdasfs");
+var_dump(value: "afdasfs"); // Retorna STRING
 
-var_dump(value: 12);
+var_dump(value: 12); // Retorna INT
 
-var_dump(value: true);
+var_dump(value: true); // Retorna BOOLEAN
 
-var_dump(value: 25.65);
+var_dump(value: 25.65); // Retorna DOUBLE
 
-var_dump(value: 2.5);
+var_dump(value: 2.5); // Retorna DOUBLE
 
-var_dump([1,2,3.6]);
+var_dump([1,2,3.6]); // Retorna ARRAY
 
 ?>
